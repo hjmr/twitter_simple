7bd3d6c05cabe6cfae44fbcca4844fcea150fb97
hjmr <2620990+hjmr@users.noreply.github.com>
2020-05-26 15:09:30 +0900

    make this work.

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ CONSUMER_KEY = os.getenv("TWITTER_CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("TWITTER_CONSUMER_SECRET")
 ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+if __name__ == "__main__":
+    print("CONSUMER_KEY = {}".format(CONSUMER_KEY))
+    print("CONSUMER_SECRET = {}".format(CONSUMER_SECRET))
+    print("ACCESS_TOKEN = {}".format(ACCESS_TOKEN))
+    print("ACCESS_TOKEN_SECRET = {}".format(ACCESS_TOKEN_SECRET))
--- get_simple.py
+++ get_simple.py
@@ -4,12 +4,6 @@ import config
 api = TwitterAPI(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
 res = api.request('tweets/search/30day/:research', {'query': 'ねこ'})
-rj = res.json()
 
-next_id = ''
-if 'next' in rj:
-    next_id = rj['next']
-
-for item in r.get_iterator():
+for item in res.get_iterator():
     print(item['id'], item['user']['screen_name'], item['user']['id'], item['text'])
-print("next:{}".format(next_id))

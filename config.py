999f89f75f342057af203b83692656fb408ed450
hjmr <2620990+hjmr@users.noreply.github.com>
2020-05-26 16:11:15 +0900

    refactor search program.

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
 
+DEV_ENV_LABEL = os.getenv("TWITTER_DEV_ENV")
+
 API_KEY = os.getenv("TWITTER_API_KEY")
 API_SECRET_KEY = os.getenv("TWITTER_API_SECRET_KEY")
 ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
 if __name__ == "__main__":
+    print("Dev environment label = {}".format(DEV_ENV_LABEL))
     print("API_KEY = {}".format(API_KEY))
     print("API_SECRET_KEY = {}".format(API_SECRET_KEY))
     print("ACCESS_TOKEN = {}".format(ACCESS_TOKEN))
--- search.py
+++ search.py
@@ -1,9 +1,47 @@
+import json
+import argparse
+
 from TwitterAPI import TwitterAPI
 import config
 
+
+API_BASE = 'tweets/search/30day/:{}'.format(config.DEV_ENV_LABEL)
 api = TwitterAPI(config.API_KEY, config.API_SECRET_KEY, config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
 
-res = api.request('tweets/search/30day/:research', {'query': 'ねこ'})
 
-for item in res.get_iterator():
-    print(item['id'], item['user']['screen_name'], item['user']['id'], item['text'])
+def parse_arg():
+    args = argparse.ArgumentParser(description="search tweets by query.")
+    args.add_argument("-f", "--filename", type=str, help="specify output JSON filename.")
+    args.add_argument("-q", "--query", type=str, help="specify query keyword.")
+    return args.parse_args()
+
+
+def search_tweets(query):
+    tweets = []
+    params = {
+        'query': query
+    }
+
+    res = api.request(API_BASE, params=params)
+    if res.status_code != 200:  # 正常通信出来なかった場合
+        print("Error with code: %d" % res.status_code)
+    else:
+        for tweet in res:
+            tweets.append(tweet)
+    return tweets
+
+
+if __name__ == '__main__':
+    args = parse_arg()
+    if args.query:
+        tweets = search_tweets(args.query)
+        if args.filename:
+            with open(args.filename, "w+") as f:
+                json.dump(tweets, f, indent=2, ensure_ascii=False)
+        else:
+            for t in tweets:
+                print("------------------------------------")
+                print("tweet id: {}".format(t['id']))
+                print("screen_name: {}".format(t['user']['screen_name']))
+                print("user id: {}".format(t['user']['id']))
+                print(t['text'])
